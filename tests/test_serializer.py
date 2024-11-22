2fba42a1c06cd697f117535c682331e375e594bc
Jonas Vacek <jvacek@users.noreply.github.com>
2024-11-22 13:49:50 +0100

    move to fixtures

--- tests/test_serializer.py
+++ tests/test_serializer.py
@@ -1,31 +1,18 @@
 from .utils import error_code_in_result, run_check
+from .utils import run_check, load_fixture_file, error_code_in_result
 
 
 def test_serializer_exclude_fails():
-    code = """
-from rest_framework import serializers
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        exclude = ('password',)
-    """
+    code = load_fixture_file('serializer_exclude.py')
     result = run_check(code)
     assert error_code_in_result("DJ26", result)
 
 
 def test_serializer_fields_all_fails():
-    code = """
-from rest_framework import serializers
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = '__all__'
-    """
+    code = load_fixture_file('serializer_fields_all.py')
     result = run_check(code)
     assert error_code_in_result("DJ27", result)
-
+    
 
 def test_serializer_fields_list_success():
     code = """
